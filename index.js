33594d8a3a94d1ce7fd4cf7085f1cc6c9302a949
Josh R <jriver35@uncc.edu>
2020-05-22 23:20:58 -0400

    Added questions and readme template

--- index.js
+++ index.js
@@ -1,12 +1,65 @@
-const questions = [
+const fs = require("fs")
+const inquirer = require("inquirer")
 
+const questions = ["What is the title of the project?", "What is your Github username?", "What is the name of your repository?",
+    "Please write a description of the project:", "Any specfic usage of the project?a", "Which license?", "If you have any contributors, what are their Github usernames?",
+    "How to test your project?", "Installation method: "
 ];
 
 function writeToFile(fileName, data) {
-}
+    
+}    
 
 function init() {
-
+    inquirer.prompt([
+        {
+            type: "input",
+            message: questions[0],
+            name: "title"
+        },
+        {
+            type: "input",
+            message: questions[1],
+            name: "username"
+        },
+        {
+            type: "input",
+            message: questions[2],
+            name: "repository"
+        },
+        {
+            type: "input",
+            message: questions[3],
+            name: "description"
+        },
+        {
+            type: "input",
+            message: questions[4],
+            name: "usage"
+        },
+        {
+            type: "input",
+            message: questions[6],
+            name: "credits"
+        },
+        {
+            type: "input",
+            message: questions[7],
+            name: "test"
+        },
+        {
+            type: "list",
+            message: questions[5],
+            name: "license",
+            choices: ["MIT", "Apache", "Apache 2", "GPL"]
+        },
+        {
+            type: "input",
+            message: questions[8],
+            name: "installation"
+        } 
+    ]
+    )
 }
 
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,41 @@
 function generateMarkdown(data) {
+  switch (data.license) {
+    case "MIT":
+      data.license = `https://img.shields.io/badge/github/license/:${data.username}/:${data.repository}`
+    break 
+  }
+  
   return `
-# ${data.title}
+  # ${data.title}
+  ![License Badge](${data.license})
 
+  ${data.description}
+
+  ## Table of Contents
+  - Installation(#installation)
+  - Usage(#usage)
+  - Credits(#credits)
+  - Tests (#tests)
+
+  ## Installation
+
+  How to install the project: 
+
+  ${data.installation}
+
+  ## Usage
+
+  ${data.usage}
+
+  ## Credits
+
+  ${data.credits}
+
+  ## Tests
+
+  How to test the project:
+
+  ${data.test}
 `;
 }
 
